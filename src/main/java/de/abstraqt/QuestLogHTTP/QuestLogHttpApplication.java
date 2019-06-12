22e02bec3693d199d57d7f7ee808dcd8826e4a55
Dominic Schmidt <ds@cluetec.de>
2019-06-12 10:07:21 +0200

    Initial commit

---
+++ src/main/java/de/abstraqt/QuestLogHTTP/QuestLogHttpApplication.java
@@ -0,0 +1,13 @@
+package de.abstraqt.QuestLogHTTP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestLogHttpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestLogHttpApplication.class, args);
+	}
+
+}
